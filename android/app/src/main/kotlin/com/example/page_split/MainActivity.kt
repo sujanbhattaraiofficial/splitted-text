34a86981c310ddcf033cb7552a16aee8fca57afd
sujan bhattarai <sujanb681@gmail.com>
2021-06-05 12:02:31 +0545

    Add final  page split

---
+++ android/app/src/main/kotlin/com/example/page_split/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.page_split
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/animation.dart';
+
+const kDuration = Duration(milliseconds: 300);
+const kCurve = Curves.easeInToLinear;
